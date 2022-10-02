3b59277e64e3fa5724cccbe0c469ff7258ac2fda
MatePataki <pataki.mate2003@gmail.com>
2022-10-03 00:14:49 +0200

    fixed navbar

--- scripts/header-changer.js
+++ scripts/header-changer.js
@@ -1,6 +1,6 @@
-let width = window.outerWidth;
+let width = window.innerWidth;
 console.log("width: " + width);
-if(width < 992) {
+if(width <= 1199.98) {
 document.getElementById("header-top").className = 'container';
 } else {
     document.getElementById("header-top").className = 'container-fluid'; 
